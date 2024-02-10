f6bece01c703f79d68dd6073401fc534fe444666
Kaung Kaung <kaunghtetaung044@gmail.com>
2024-02-11 02:18:35 +0630

    AdoDotNetExample

---
+++ KHADotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -0,0 +1,138 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace KHADotNetCore.ConsoleApp.AdoDotNetExamples
+{
+    public class AdoDotNetExample
+    {
+        private SqlConnectionStringBuilder sqlConnectionStringBuilder;
+        private SqlConnection sqlConnection;
+
+        private string query = "";
+        public AdoDotNetExample()
+        {
+            sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = "DESKTOP-V1H7OM6";
+            sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+            sqlConnectionStringBuilder.UserID = "sa";
+            sqlConnectionStringBuilder.Password = "KaungKaung";
+
+            sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public void Read()
+        {
+            query = "SELECT * FROM Tbl_Blog";
+
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            sqlConnection.Close();
+
+            //DataSet => accept many tables
+            //DataTable => each table
+            //DataRow
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine($"BlodId {dr["BlogId"]}");
+                Console.WriteLine("BlogTitle " + dr["BlogTitle"]);
+                Console.WriteLine("Author " + dr["BlogAuthor"]);
+                Console.WriteLine("BlogContent " + dr["BlogContent"]);
+                Console.WriteLine();
+            }
+        }
+
+        public void Edit(int id)
+        {
+            query = "SELECT * FROM Tbl_Blog WHERE BlogId = @BlogId";
+
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            sqlConnection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No  data found");
+                return;
+            }
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine($"BlodId " + dr["BlogId"]);
+                Console.WriteLine("BlogTitle " + dr["BlogTitle"]);
+                Console.WriteLine("Author " + dr["BlogAuthor"]);
+                Console.WriteLine("BlogContent " + dr["BlogContent"]);
+                Console.WriteLine();
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            query = @"INSERT INTO [dbo].[Tbl_Blog]
+                    (
+                        [BlogTitle],
+                        [BlogAuthor],
+                        [BlogContent]
+                    )
+                    VALUES
+                    (
+                        @BlogTitle,
+                        @BlogAuthor,
+                        @BlogContent
+                    )";
+
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            string message = result > 0 ? "Saving Successful." : "Saving Failed";
+            Console.WriteLine(message);
+        }
+
+        public void Update(int id, string title, string author, string content)
+        {
+            query = @"UPDATE [dbo].[Tbl_Blog] SET 
+                    [BlogTitle] = @BlogTitle,
+                    [BlogAuthor] = @BlogAuthor,
+                    [BlogContent] = @BlogContent
+                    WHERE BlogId = @BlogId";
+
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            int result = cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            string message = result > 0 ? "Updating Successful." : "Updating Failed";
+            Console.WriteLine(message);
+        }
+
+        public void Delete(int id)
+        {
+            query = @"DELETE FROM [dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
+
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            int result = cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            string message = result > 0 ? "Delete Successful." : "Delete Failed";
+            Console.WriteLine(message);
+        }
+    }
+}
--- KHADotNetCore.ConsoleApp/Program.cs
+++ KHADotNetCore.ConsoleApp/Program.cs
@@ -1,44 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Data;
-using System.Data.SqlClient;
+using KHADotNetCore.ConsoleApp.AdoDotNetExamples;
 
 Console.WriteLine("Hello, World!");
-//Console.ReadLine();
-//Console.ReadKey();
 
-#region Read
-
-SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
-sqlConnectionStringBuilder.DataSource = "DESKTOP-V1H7OM6";
-sqlConnectionStringBuilder.InitialCatalog = "TestDb";
-sqlConnectionStringBuilder.UserID = "sa";
-sqlConnectionStringBuilder.Password = "KaungKaung";
-
-string query = "SELECT * FROM Tbl_Blog";
-SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-sqlConnection.Open();
-
-SqlCommand cmd = new SqlCommand(query, sqlConnection);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-
-
-DataTable dt = new DataTable();
-adapter.Fill(dt);
-
-sqlConnection.Close();
-
-//DataSet => accept many tables
-//DataTable => each table
-//DataRow
-
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine(dr["BlogId"]);
-    Console.WriteLine(dr["BlogTitle"]);
-    Console.WriteLine(dr["BlogAuthor"]);
-    Console.WriteLine(dr["BlogContent"]);
-    Console.WriteLine();
-}
-#endregion
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Edit(2);
+//adoDotNetExample.Create("Testing Title", "Testing Author", "Testing Content");
+adoDotNetExample.Update(1004,"Testing Title 1004", "Testing Author 1004", "Testing Content 1004");
+adoDotNetExample.Delete(1003);
+adoDotNetExample.Read();
 
 Console.ReadKey();
