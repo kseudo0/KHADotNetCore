ce21a6a926b6e212d31a9596c4d3eb3f5395a570
Kaung Kaung <kaunghtetaung044@gmail.com>
2024-02-11 00:30:01 +0630

    Add project files.

---
+++ KHADotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+//Console.ReadKey();
+
+#region Read
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "DESKTOP-V1H7OM6";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "KaungKaung";
+
+string query = "SELECT * FROM Tbl_Blog";
+SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+sqlConnection.Open();
+
+SqlCommand cmd = new SqlCommand(query, sqlConnection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+sqlConnection.Close();
+
+//DataSet => accept many tables
+//DataTable => each table
+//DataRow
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+    Console.WriteLine();
+}
+#endregion
+
+Console.ReadKey();
